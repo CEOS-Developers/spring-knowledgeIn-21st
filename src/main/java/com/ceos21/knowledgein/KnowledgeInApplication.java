efd043cc73231896c153d106928878910e46a8ea
Gothax <scott990328@gmail.com>
2025-03-19 15:37:58 +0900

    feat: init project

---
+++ src/main/java/com/ceos21/knowledgein/KnowledgeInApplication.java
@@ -0,0 +1,13 @@
+package com.ceos21.knowledgein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeInApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ceos21/knowledgein/KnowledgeInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceos21.knowledgein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
