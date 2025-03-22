455ccbbef0059dd2f94d9705d426ca4358bf4017
wlqgkrry <wlqgkrry@gmail.com>
2025-03-22 14:04:57 +0900

    add : 초기 개발 설정 관련 파일 추가

---
+++ spring-boot/src/main/java/com/ceos21/spring_boot/SpringKnowledgeApplication.java
@@ -0,0 +1,12 @@
+package com.ceos21.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringKnowledgeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKnowledgeApplication.class, args);
+    }
+}
