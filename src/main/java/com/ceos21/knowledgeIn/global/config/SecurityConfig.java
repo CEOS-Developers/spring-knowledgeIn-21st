1ef4bb5208ce8ec6c64a86cd949bde8b51de9124
challonsy <musain2019@naver.com>
2025-04-27 18:47:23 +0900

    feat : create security config

---
+++ src/main/java/com/ceos21/knowledgeIn/global/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.ceos21.knowledgeIn.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((auth)->auth
+                .requestMatchers("").permitAll()
+                .requestMatchers("/").hasRole("ADMIN")
+                .requestMatchers("").hasAnyRole("USER", "ADMIN")
+                .anyRequest().authenticated()
+        );
+
+        return http.build();
+
+    }
+}
