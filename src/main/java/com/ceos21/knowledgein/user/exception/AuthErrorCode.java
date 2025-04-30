4d0c6a8c4fb579e66c1e19d83a483fb2841d66f7
Gothax <scott990328@gmail.com>
2025-04-30 23:03:53 +0900

    feat: security 예외 추가
    - 책임을 나누기 위해 AuthErrorCode 일부를 TokenException으로 이동

--- src/main/java/com/ceos21/knowledgein/user/exception/AuthErrorCode.java
+++ src/main/java/com/ceos21/knowledgein/user/exception/AuthErrorCode.java
@@ -9,10 +9,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum AuthErrorCode implements ErrorCode {
 
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
-    INVALID_SIGNATURE(HttpStatus.UNAUTHORIZED, "유효하지 않은 서명입니다."),
-    REFRESH_EXPIRED(HttpStatus.UNAUTHORIZED, "로그인이 만료되었습니다. 다시 로그인해 주세요.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
