3c3835f353582ad51e9a05655697a08b021ff900
SeoyeonPark1223 <trispark1223@gmail.com>
2025-05-17 21:46:13 +0900

    Chore: 도커 실험

--- src/main/java/com/knowledgein/springboot/service/imageService/ImageCommandService.java
+++ src/main/java/com/knowledgein/springboot/service/imageService/ImageCommandService.java
@@ -1,6 +1,5 @@
 package com.knowledgein.springboot.service.imageService;
 
-import com.knowledgein.springboot.web.dto.imageDTO.ImageRequestDTO;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageCommandService {
